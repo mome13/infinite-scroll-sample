6a9f7ae9e260ba3ff383c49b1a0d75fceb317900
mehran13mome <mehran13mome@gmail.com>
2024-01-18 01:59:14 +0330

    Feat: Add anime with server actions

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
+import { fetchAnime } from './action';
+import AnimeCard, { AnimeProp } from '@/components/AnimeCard';
 import Image from 'next/image';
 
-export default function Home() {
+export default async function Home() {
+  const data = await fetchAnime(1);
   return (
     <>
-      <main className='sm:p-16 py-16 px-8 flex flex-col gap-10 bg-[#0F1117]'></main>
+      <main className='sm:p-16 py-16 px-8 flex flex-col gap-10 bg-[#0F1117]'>
+        <h1 className='text-white text-3xl font-semibold'>Explore Anime</h1>
+
+        <section className='grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10'>
+          {data.map((item: AnimeProp, index: number) => (
+            <AnimeCard key={item.id} anime={item} index={index} />
+          ))}
+        </section>
+      </main>
     </>
   );
 }
